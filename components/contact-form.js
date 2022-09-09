c76a86a7c13dff41a31722f5162825db48d88968
Max Pelletier <max@nuube.ca>
2022-09-08 22:59:17 -0400

    wip

--- components/contact-form.js
+++ components/contact-form.js
@@ -11,25 +11,64 @@ export default function ContactForm() {
 
   return (
     <form className="flex flex-col space-y-4 p-4" onSubmit={handleSubmit}>
-      <label className="label-text" htmlFor="email">
+      <span className="pb-2">Collaboration request</span>
+      {/* <label className="label-text" htmlFor="email">
         Email Address
-      </label>
+      </label> */}
+      <input
+        className="input input-bordered"
+        placeholder="Name"
+        id="name"
+        type="text"
+        name="name"
+        required
+      />
+      <ValidationError prefix="Name" field="name" errors={state.errors} />
+
       <input
         className="input input-bordered"
         placeholder="email Address"
         id="email"
         type="email"
         name="email"
+        required
       />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <label className="label-text" htmlFor="email">
-        Message
-      </label>
+      <div>
+        <input
+          type="radio"
+          id="brand"
+          name="collaboType"
+          value="brand"
+          className="accent-fuchsia-600"
+          defaultChecked={true}
+        />
+        <label for="brand">Brand collaboration</label>
+      </div>
+      <div>
+        <input
+          type="radio"
+          id="content"
+          name="collaboType"
+          value="content"
+          className="accent-fuchsia-600"
+        />
+        <label for="content">Content collaboration</label>
+      </div>
+
+      {/* <label className="label-text" htmlFor="message">
+        More information
+      </label> */}
+      <div className="text-sm font-thin">
+        **For content collaboration, please provide your Instagram page address
+        in the section below.
+      </div>
       <textarea
         className="textarea textarea-bordered"
-        placeholder="message"
+        placeholder="More information"
         id="message"
         name="message"
+        required
       />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
       <button
