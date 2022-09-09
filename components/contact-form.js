d214e7e42b82bdcb45365219bdcaa06056dc76a5
Max Pelletier <max@nuube.ca>
2022-09-08 21:45:51 -0400

    wip

--- components/contact-form.js
+++ components/contact-form.js
@@ -1,7 +1,9 @@
 import { useForm, ValidationError } from "@formspree/react";
 
 export default function ContactForm() {
-  const [state, handleSubmit] = useForm("YOUR_FORM_ID");
+  const [state, handleSubmit] = useForm(
+    process.env.NEXT_PUBLIC_FORMSPREE_FORM_ID
+  );
 
   if (state.succeeded) {
     return <p>Thanks for your submission!</p>;
@@ -9,16 +11,35 @@ export default function ContactForm() {
 
   return (
     <form className="flex flex-col space-y-4 p-4" onSubmit={handleSubmit}>
-      <label className="label-text" htmlFor="email">Email Address</label>
-      <input className="input input-bordered" placeholder="email Address" id="email" type="email" name="email" />
+      <label className="label-text" htmlFor="email">
+        Email Address
+      </label>
+      <input
+        className="input input-bordered"
+        placeholder="email Address"
+        id="email"
+        type="email"
+        name="email"
+      />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
-      <label className="label-text" htmlFor="email">Message</label>
-      <textarea className="textarea textarea-bordered" placeholder="message" id="message" name="message" />
+      <label className="label-text" htmlFor="email">
+        Message
+      </label>
+      <textarea
+        className="textarea textarea-bordered"
+        placeholder="message"
+        id="message"
+        name="message"
+      />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
-      <button className='btn btn-secondary' type="submit" disabled={state.submitting}>
+      <button
+        className="btn btn-secondary"
+        type="submit"
+        disabled={state.submitting}
+      >
         Submit
       </button>
       <ValidationError errors={state.errors} />
-    </form>  
+    </form>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
-import type { NextPage } from 'next'
-import React, { useState } from "react";
-import Image from 'next/image'
-import Link from 'next/link'
+import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
-import { BsInstagram, BsTwitter } from 'react-icons/bs';
-import { FaTiktok } from 'react-icons/fa';
-import { SiOnlyfans } from 'react-icons/si';
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import { BsInstagram, BsTwitter } from "react-icons/bs";
+import { FaTiktok } from "react-icons/fa";
+import { SiOnlyfans } from "react-icons/si";
 import ContactForm from "../components/contact-form";
 
 const Home: NextPage = () => {
-    
   return (
     <>
       <NextSeo
@@ -27,10 +26,10 @@ const Home: NextPage = () => {
             priority
             src="/images/background.jpg"
           />
-          <div className="z-10 w-full md:w-72 h-full flex flex-col items-center justify-between text-center text-xl md:text-5xl font-extrabold text-white">
+          <div className="z-10 w-full md:w-72 h-full flex flex-col items-center justify-around text-center text-white">
             <div className="z-10 h-40 w-40 md:h-56 md:w-56 relative border-4 border-fuchsia-600 rounded-full">
               <Image
-                src={'/images/avatar.jpg'}
+                src={"/images/avatar.jpg"}
                 layout="fill"
                 objectPosition="top"
                 objectFit="cover"
@@ -39,23 +38,60 @@ const Home: NextPage = () => {
               />
             </div>
             <div>
-              <h2 className='text-3xl md:text-6xl autobiography'>Noémie</h2>
-              <h3 className='text-sm md:text-3xl'>DUFRESNE</h3>
+              <h2 className="text-7xl md:text-9xl autobiography">Noémie</h2>
+              <h3 className="text-md md:text-3xl">DUFRESNE</h3>
             </div>
-            <div className='flex space-x-3'>
-              <div className='bg-fuchsia-600 p-2 md:p-4 rounded-full cursor-pointer'><Link href="https://www.instagram.com/noemie.dufresne"><span><BsInstagram /></span></Link></div>
-              <div className='bg-fuchsia-600 p-2 md:p-4 rounded-full cursor-pointer'><Link href="https://twitter.com/dufresnenoemie1"><span><BsTwitter /></span></Link></div>
-              <div className='bg-fuchsia-600 p-2 md:p-4 rounded-full cursor-pointer'><Link href="https://www.tiktok.com/@noemie_dufresne"><span><FaTiktok /></span></Link></div>
+            <div className="flex space-x-3">
+              <div className="bg-fuchsia-600 p-2 md:p-4 rounded-full cursor-pointer">
+                <Link href="https://www.instagram.com/noemie.dufresne">
+                  <span>
+                    <BsInstagram />
+                  </span>
+                </Link>
+              </div>
+              <div className="bg-fuchsia-600 p-2 md:p-4 rounded-full cursor-pointer">
+                <Link href="https://twitter.com/dufresnenoemie1">
+                  <span>
+                    <BsTwitter />
+                  </span>
+                </Link>
+              </div>
+              <div className="bg-fuchsia-600 p-2 md:p-4 rounded-full cursor-pointer">
+                <Link href="https://www.tiktok.com/@noemie_dufresne">
+                  <span>
+                    <FaTiktok />
+                  </span>
+                </Link>
+              </div>
             </div>
-            <div className='flex items-center space-x-4 w-full justify-center bg-white text-fuchsia-600 p-2 md:p-4 rounded-full cursor-pointer'>
-              <div className='text-2xl md:text-5xl'><Link href="https://onlyfans.com/noemiedufresne"><span><SiOnlyfans /></span></Link></div>
-              <div className='text-lg md:text-xl'>Onlyfans</div>
+            <div className=" text-lg md:text-xl w-full  bg-white text-fuchsia-600 p-2 md:p-4 rounded-full cursor-pointer">
+              <Link href="https://onlyfans.com/noemiedufresne">
+                <div className="flex justify-center items-center space-x-4">
+                  <SiOnlyfans />
+                  <span>Onlyfans</span>
+                </div>
+              </Link>
             </div>
-            <div className='flex items-center space-x-4 w-full justify-center bg-white text-fuchsia-600 p-2 md:p-4 rounded-full cursor-pointer'>
-              <div className='text-lg md:text-xl'><Link href="https://mym.fans/Noemiedufresne">MYM</Link></div>
+            <div className=" text-lg md:text-xl  w-full  bg-white text-fuchsia-600 p-3 md:p-4 rounded-full cursor-pointer">
+              <Link href="https://mym.fans/Noemiedufresne">
+                <div className="flex justify-center items-center space-x-4">
+                  <img src="/images/mym.svg" className=" w-14" />
+                  {/* <span>MYM</span> */}
+                </div>
+              </Link>
             </div>
-            <p className='text-[12px] leading-3 lg:text-xs text-justify'>Content creator for the past ten years, Noémie Dufresne stands out for the originality of her various concepts and the quality of her content. Her avant-garde style and lifestyle perfectly represent the glamour of everyday's life.</p>
-            <label htmlFor="my-modal-3" className="text-lg md:text-xl bg-fuchsia-600 w-full py-2 md:py-5 rounded-full cursor-pointer">Collaboration</label>
+            <p className="text-[12px] leading-3 lg:text-xs text-justify">
+              Content creator for the past ten years, Noémie Dufresne stands out
+              for the originality of her various concepts and the quality of her
+              content. Her avant-garde style and lifestyle perfectly represent
+              the glamour of everyday's life.
+            </p>
+            <label
+              htmlFor="my-modal-3"
+              className="text-lg md:text-xl bg-fuchsia-600 w-full py-2 md:py-5 rounded-full cursor-pointer"
+            >
+              Collaboration
+            </label>
           </div>
         </div>
       </div>
@@ -63,13 +99,18 @@ const Home: NextPage = () => {
         <input type="checkbox" id="my-modal-3" className="modal-toggle" />
         <div className="modal">
           <div className="modal-box relative">
-            <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+            <label
+              htmlFor="my-modal-3"
+              className="btn btn-sm btn-circle absolute right-2 top-2"
+            >
+              ✕
+            </label>
             <ContactForm />
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
