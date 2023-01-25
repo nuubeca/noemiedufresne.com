d087aee9a38c4ec60192c9a381247734e6c2f2a6
Maxime Pelletier <tech@solutionprets.ca>
2023-01-24 20:59:38 -0500

    add new meetfans video

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,7 +68,9 @@ const Home: NextPage = () => {
             </Link> */}
             <div className=" text-lg md:text-xl w-full font-extrabold  bg-white text-fuchsia-600 p-2 md:p-3 rounded-full cursor-pointer">
               <Link href="https://onlyfans.com/noemiedufresne">
-                <div className="flex justify-center items-center space-x-4">
+                <div className="flex justify-center items-center space-x-4 text-md">
+                  <span className="-rotate-45 text-slate-700">#New MeetFans video</span>
+
                   {/* <SiOnlyfans />
                   <span>Onlyfans</span>
                    */}
